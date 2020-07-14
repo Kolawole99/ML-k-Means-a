9a6c51c600292f1ed3e3015d243174016ff7c154
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-14 14:23:50 +0100

    Insights: Scatter plot of the algorithm classification

--- k-means.py
+++ k-means.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 from sklearn.cluster import KMeans 
 from sklearn.preprocessing import StandardScaler
+from mpl_toolkits.mplot3d import Axes3D 
 #%matplotlib inline #needed in jupyter notebooks
 
 
@@ -22,8 +23,8 @@ print(df)
 
 #========================================Preprocessing======================================
 df = cust_df.drop('Address', axis=1)
-df = df.head()
-print(df)
+df_view = df.head()
+print(df_view)
 
 #=================================Normalizing over standard deviation==============================
 X = df.values[:,1:]
@@ -46,7 +47,8 @@ print(labels)
 
 #================================Assign labels to each row in the dataframe==========================
 df["Clus_km"] = labels
-df.head(5)
+label_view = df.head(5)
+print(label_view)
 
 #==============================Checking for the centroid values by average============================
 centroid = df.groupby('Clus_km').mean()
@@ -59,3 +61,15 @@ plt.xlabel('Age', fontsize=18)
 plt.ylabel('Income', fontsize=16)
 plt.show()
 
+#==============================Plotting the image of the scatter plot=============================
+fig = plt.figure(1, figsize=(8, 6))
+plt.clf()
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+plt.cla()
+# plt.ylabel('Age', fontsize=18)
+# plt.xlabel('Income', fontsize=16)
+# plt.zlabel('Education', fontsize=16)
+ax.set_xlabel('Education')
+ax.set_ylabel('Age')
+ax.set_zlabel('Income')
+ax.scatter(X[:, 1], X[:, 0], X[:, 3], c= labels.astype(np.float))
