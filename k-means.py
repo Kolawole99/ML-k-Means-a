d5bfecaa67ff2e06ba3a4e67bbc212a6b05631bf
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-14 13:23:44 +0100

    Importing necessary Libraries

--- k-means.py
+++ k-means.py
@@ -1,1 +1,10 @@
-#===========================IMPORTING NEEDED LINRARIES================================
+#=========-=============================IMPORTING NEEDED LINRARIES=======================================
+import random 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn.cluster import KMeans 
+from sklearn.datasets.samples_generator import make_blobs 
+import pandas as pd
+#%matplotlib inline #needed in jupyter notebooks
+
+
