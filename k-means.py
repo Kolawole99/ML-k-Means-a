356f33848303137abfca38b86b6347361456687f
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-14 13:37:23 +0100

    Data Preprocessing

--- k-means.py
+++ k-means.py
@@ -1,10 +1,10 @@
 #=========-=============================IMPORTING NEEDED LINRARIES=======================================
 import random 
 import numpy as np 
+import pandas as pd
 import matplotlib.pyplot as plt 
 from sklearn.cluster import KMeans 
-from sklearn.datasets.samples_generator import make_blobs 
-import pandas as pd
+from sklearn.preprocessing import StandardScaler
 #%matplotlib inline #needed in jupyter notebooks
 
 
@@ -14,3 +14,12 @@ import pandas as pd
 cust_df = pd.read_csv("Cust_Segmentation.csv")
 df = cust_df.head()
 print(df)
+
+
+#=======================================DATA PREPROCESSING======================================
+
+#========================================Preprocessing======================================
+df = cust_df.drop('Address', axis=1)
+df = df.head()
+print(df)
+
